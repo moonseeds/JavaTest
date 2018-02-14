8521f7a6a8cf805d8ff83211cffe276f9c3067e2
rebecca_dillon <rebeccadillon214@gmail.com>
2018-02-14 16:30:33 +0000

    Tune.java getters and toString method added

--- ie/dit/Tune.java
+++ ie/dit/Tune.java
@@ -1,5 +1,36 @@
+package ie.dit;
+
 public class Tune 
 {
+	private int x;
+	private String title;
+	private String altTitle;
+	private String notation;
 	
+	public int getX()
+	{
+		return x;
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	public String getAltTitle()
+	{
+		return altTitle;
+	}
+	
+	public getNotation()
+	{
+		return notation;
+	}
+	
+	public String toString()
+	{
+		
+		return x+","+title+","+altTitle;
+	}
 	
 }
